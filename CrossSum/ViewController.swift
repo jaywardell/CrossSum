2d5d67b306071a3b4532a9df8e796358ecbdaa62
jaywardell <jaywardell@mac.com>
2018-09-24 18:00:18 -0400

    Initial Commit

---
+++ CrossSum/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CrossSum
+//
+//  Created by Joseph Wardell on 9/24/18.
+//  Copyright © 2018 Joseph Wardell. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
