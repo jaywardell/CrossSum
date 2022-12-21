8be65726b1e3a74616abee81205f4a3b1b8c1521
Joseph Wardell <jaywardell@mac.com>
2022-12-21 09:09:19 -0500

    GameViewContorller shows the hint button whenever the game is playing or advancing, but disables it unless there's an available hint

--- CrossSum/Controller/GameViewController.swift
+++ CrossSum/Controller/GameViewController.swift
@@ -193,15 +193,15 @@ class GameViewController: UIViewController {
             ToggleBasedOnStatePresenter(gamePlayView.scoreLabel, Round.State.allPlaying + [.advancing, .paused]),
 
             // the hint UI should be visible unless the round is paused or there are no hints
-            ToggleBasedOnStatePresenter(gamePlayView.hintButton, [.advancing, .playing(hasHints: true)]),
-            ToggleBasedOnStatePresenter(gamePlayView.hintTally, [.advancing, .playing(hasHints: true)]),
+            ToggleBasedOnStatePresenter(gamePlayView.hintButton, Round.State.allPlaying + [.advancing]),
+            ToggleBasedOnStatePresenter(gamePlayView.hintTally, Round.State.allPlaying + [.advancing]),
             
             // the skip UI should be visible unless the round is paused
             ToggleBasedOnStatePresenter(gamePlayView.skipButton, Round.State.allPlaying + [.advancing]),
             ToggleBasedOnStatePresenter(gamePlayView.skipTally, Round.State.allPlaying + [.advancing]),
  
             // but when the round is advancing, the buttons shouldn't be enabled
-            ToggleBasedOnStatePresenter(ToggleableKeyedPresenter(gamePlayView.hintButton, key: \UIButton.isEnabled), Round.State.allPlaying),
+            ToggleBasedOnStatePresenter(ToggleableKeyedPresenter(gamePlayView.hintButton, key: \UIButton.isEnabled), [.playing(hasHints: true)]),
             ToggleBasedOnStatePresenter(ToggleableKeyedPresenter(gamePlayView.skipButton, key: \UIButton.isEnabled), Round.State.allPlaying),
             
             // except the play_pause button should be enabled when the round is paused also
